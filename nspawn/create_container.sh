744d40097a71324dff652c00d2529f6afa9fb585
tomato-tom <hajimeee49@gmail.com>
2025-07-16 22:00:08 +0900

    update

--- nspawn/create_container.sh
+++ nspawn/create_container.sh
@@ -10,12 +10,14 @@ else
 fi
 
 if [ $# -eq 0 ]; then
-    log info "Usage: $0 <container_name> [base_tar]"
+    log info "Usage: $0 <container_name> [base_tar][description]"
     exit 1
 fi
 
 CONTAINER_NAME=$1
+# OS_TYPE: jammy, bookwormのようなのがやりやすいか
 BASE_TAR=${2:-"/srv/nspawn_images/stable-base-rootfs.tar.gz"}
+DESCRIPTION=$3
 CONTAINER_DIR="/var/lib/machines/$CONTAINER_NAME"
 
 if [ ! -f "$BASE_TAR" ]; then
@@ -30,6 +32,17 @@ sudo tar -xzf "$BASE_TAR" -C "$CONTAINER_DIR"
 
 # Update hostname
 sudo chroot $CONTAINER_DIR bash -c "echo $CONTAINER_NAME > /etc/hostname"
+
+# メタデータ作成
+META_DIR="/var/lib/machines/.meta"
+sudo mkdir -p "$META_DIR"
+
+sudo bash -c "cat > \"$META_DIR/$CONTAINER_NAME.conf\" <<EOL
+CONTAINER_NAME=\"$CONTAINER_NAME\"
+OS_TYPE=\"$OS_TYPE\"
+DESCRIPTION=\"$DESCRIPTION\"
+CREATED_DATE=\"$(date +%Y-%m-%d)\"
+EOL"
 
 log info "Container $CONTAINER_NAME created at $CONTAINER_DIR"
 
---
+++ nspawn/run_container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+#コンテナ起動スクリプト
+# ./run_container.sh c1
+#
